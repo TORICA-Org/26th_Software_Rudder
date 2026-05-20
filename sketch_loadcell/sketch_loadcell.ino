3953af8d23ce7dd02bcd272985034133784bf9b0
mizuno-1610 <8425100@ed.tus.ac.jp>
2026-05-20 20:44:32 +0900

    test by samd21

--- sketch_loadcell/sketch_loadcell.ino
+++ sketch_loadcell/sketch_loadcell.ino
@@ -1,52 +1,62 @@
 #include <IcsHardSerialClass.h>
-const byte EN_PIN = 4;
-IcsHardSerialClass krs(&Serial, EN_PIN);
+const byte EN_PIN = 2;
+IcsHardSerialClass krs(&Serial1, EN_PIN);
 
-#define loadcell_L A0
-#define loadcell_R A1
+#define loadcell_L A7
+#define loadcell_R A8
 
 const float neutral_pos = 7500.0;
 const float maxangle_val = (15.0 / 135.0) * 4000.0;
 
+float deadzone = 3.0; //[%]
+
 void setup() {
+  analogReadResolution(12);
   pinMode(loadcell_L, INPUT);
   pinMode(loadcell_R, INPUT);
-  krs.begin(115200, 10);
+  krs.begin();
   Serial.begin(115200);
+  delay(3000);
+  Serial.println("begin");
 }
 
 void loop() {
   float valL = analogRead(loadcell_L);
   float valR = analogRead(loadcell_R);
 
+  Serial.println("here0");
+
   if (valL < 0) valL = 0;
   if (valR < 0) valR = 0;
 
-  float average = (valL + valR) / 2.0;
+  Serial.println("here1");
 
-  float rawControl = 0;
+
+  float percentage = 0;
   float total = valL + valR;
+  
+  Serial.println("here2");
 
-  if (total > 20.0){
-    rawControl = 100.0 * (valL - valR) / total;
-  }
-  else{
-    rawControl = 0;
-  }
+  percentage = 100.0 * (valL - valR) / total;
+ 
 
-  if (abs(rawControl) < 3.0) rawControl = 0;
+  if (abs(percentage) < deadzone) percentage = 0;
+  Serial.println("here3");
 
-  float outputOffset = rawControl * (maxangle_val / 100.0);
+  float outputOffset = maxangle_val * (percentage / 100.0);
 
-  long target_pos = (long)(neutral_pos + outputOffset);
+  unsigned int target_pos = (unsigned int)(neutral_pos + outputOffset);
 
-  krs.setPos(0, target_pos);
+  Serial.println("here4;");
+
+  //krs.setPos(0, target_pos);
+
+  Serial.println("here5");
 
   Serial.print("Left:"); Serial.print(valL);
   Serial.print("Right:"); Serial.print(valR);
   Serial.print("LR_Total:"); Serial.print(total);
-  Serial.print("Avg:"); Serial.print(average);
-  Serial.print("Control:"); Serial.print(rawControl);
+  Serial.print("Control:"); Serial.print(percentage);
 
   Serial.print("Target:"); Serial.println(target_pos);
 
