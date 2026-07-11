dc61b910b013b23d69e8409f424d55ea8b5e1f5c
00kenno <114tukaiyasui@gmail.com>
2026-07-11 09:29:49 +0900

    last full-assembly

--- sketch_loadcell/sketch_loadcell.ino
+++ sketch_loadcell/sketch_loadcell.ino
@@ -1,19 +1,28 @@
 #include <IcsHardSerialClass.h>
-const byte EN_PIN = 2;
-IcsHardSerialClass krs(&Serial1, EN_PIN);
 
-#define loadcell_L A7
-#define loadcell_R A8
+constexpr uint8_t LOADCELL_L = A0;
+constexpr uint8_t LOADCELL_R = A1;
+constexpr uint8_t POT = A3;
 
-const float neutral_pos = 7500.0;
-const float maxangle_val = (15.0 / 135.0) * 4000.0;
+constexpr int NEUTRAL_POS = 7500;
+constexpr float MAX_ANGLE = 15/2;
+
+const uint8_t EN_PIN = D2;
+const int BAUDLATE = 115200;
+const int TIMEOUT = 100;
+SerialPIO myserial(D7, D6);
+IcsHardSerialClass krs(&myserial, EN_PIN, BAUDLATE, TIMEOUT);
+
+constexpr float loadcell_min = 1000;
+constexpr float loadcell_max = 2300;
 
 float deadzone = 3.0; //[%]
 
 void setup() {
   analogReadResolution(12);
-  pinMode(loadcell_L, INPUT);
-  pinMode(loadcell_R, INPUT);
+  pinMode(LOADCELL_L, INPUT);
+  pinMode(LOADCELL_R, INPUT);
+  pinMode(POT, INPUT);
   krs.begin();
   Serial.begin(115200);
   delay(3000);
@@ -21,44 +30,51 @@ void setup() {
 }
 
 void loop() {
-  float valL = analogRead(loadcell_L);
-  float valR = analogRead(loadcell_R);
-
-  Serial.println("here0");
+  float valL = analogRead(LOADCELL_L) - loadcell_min;
+  float valR = analogRead(LOADCELL_R) - loadcell_min;
+  float trim = ((float) analogRead(POT) - 2048.0) / 4096.0 * 1500.0;
 
   if (valL < 0) valL = 0;
   if (valR < 0) valR = 0;
 
-  Serial.println("here1");
+  float percentageL = valL / (loadcell_max - loadcell_min) * 100;
+  float percentageR = valR / (loadcell_max - loadcell_min) * 100;
 
+  float percentageSum = percentageR - percentageL;
 
-  float percentage = 0;
-  float total = valL + valR;
-  
-  Serial.println("here2");
+  if (abs(percentageSum) < deadzone) percentageSum = 0;
+  // Serial.println("here3");
 
-  percentage = 100.0 * (valL - valR) / total;
- 
+  float outputOffset = MAX_ANGLE / 135 * 4000 * (percentageSum / 100.0);
 
-  if (abs(percentage) < deadzone) percentage = 0;
-  Serial.println("here3");
+  int target_pos = NEUTRAL_POS + trim + (int)(outputOffset);
 
-  float outputOffset = maxangle_val * (percentage / 100.0);
+  // Serial.println("here4");
 
-  unsigned int target_pos = (unsigned int)(neutral_pos + outputOffset);
+  krs.setPos(0, target_pos);
 
-  Serial.println("here4;");
+  // Serial.println("here5");
 
-  //krs.setPos(0, target_pos);
+  // Serial.print("Left:"); 
+  Serial.print(valL);
+  Serial.print(", ");
+  // Serial.print("Right:"); 
+  Serial.print(valR);
+  Serial.print(", ");
+  Serial.print(trim);
+  Serial.print(", ");
+  Serial.print(percentageSum);
+  Serial.print(", ");
+  Serial.print(target_pos);
+  Serial.println();
 
-  Serial.println("here5");
+  // Serial.print("LR_Total:"); 
+  // Serial.print(total);
+  // Serial.print("Control:"); 
+  // Serial.print(percentage);
 
-  Serial.print("Left:"); Serial.print(valL);
-  Serial.print("Right:"); Serial.print(valR);
-  Serial.print("LR_Total:"); Serial.print(total);
-  Serial.print("Control:"); Serial.print(percentage);
+  // Serial.print("Target:"); 
+  // Serial.println(target_pos);
 
-  Serial.print("Target:"); Serial.println(target_pos);
-
-  delay(50);
+  delay(10);
 }
